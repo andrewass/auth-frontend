443695a5aa8952f2a5fa1f6bb9574f0293ba61cd
Andreas Wassum <a.wassum@gmail.com>
2024-01-28 01:44:36 +0100

    Using session to toggle between sign in and out

---
+++ src/app/components/NavigationMenu.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import {signIn, signOut, useSession} from "next-auth/react";
+
+
+const AuthButton = () => {
+    const {data: session} = useSession();
+
+    if(session){
+        return (
+            <button onClick={() => signOut()}>Sign Out</button>
+        );
+    } else {
+        return (
+            <button onClick={() => signIn()}>Sign In</button>
+        );
+    }
+}
+
+
+const NavigationMenu = () => {
+    return (
+        <div>
+            <AuthButton/>
+        </div>
+    );
+}
+
+export default NavigationMenu;
