8edf36eb837f2b28b322346c8473914c1ffb1add
Andreas Wassum <a.wassum@gmail.com>
2025-12-07 21:06:48 +0100

    Redesign of client registration page

--- src/app/clients/registration/GeneralSettings.tsx
+++ src/app/clients/registration/GeneralSettings.tsx
@@ -1,3 +1,34 @@
 export default function GeneralSettings() {
-	return <div>General Settings</div>;
+	return (
+		<div className="ml-20">
+			<fieldset className="fieldset">
+				<legend className="fieldset-legend">Client Id</legend>
+				<input
+					type="text"
+					className="input w-full max-w-xs sm:max-w-md lg:max-w-lg"
+				/>
+			</fieldset>
+			<fieldset className="fieldset">
+				<legend className="fieldset-legend">Client Id</legend>
+				<input
+					type="text"
+					className="input w-full max-w-xs sm:max-w-md lg:max-w-lg"
+				/>
+			</fieldset>
+			<fieldset className="fieldset">
+				<legend className="fieldset-legend">Client Id</legend>
+				<input
+					type="text"
+					className="input w-full max-w-xs sm:max-w-md lg:max-w-lg"
+				/>
+			</fieldset>
+			<fieldset className="fieldset">
+				<legend className="fieldset-legend">Your bio</legend>
+				<textarea
+					className="textarea h-24 w-full max-w-xs sm:max-w-md lg:max-w-lg"
+					placeholder="Bio"
+				></textarea>
+			</fieldset>
+		</div>
+	);
 }
--- src/app/clients/registration/page.tsx
+++ src/app/clients/registration/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Link from "next/link";
 import { redirect } from "next/navigation";
 import { useState } from "react";
 import CapabilityConfig from "@/app/clients/registration/CapabilityConfig";
@@ -11,61 +10,58 @@ export default function ClientRegistrationPage() {
 	const [currentStep, setCurrentStep] = useState<number>(1);
 
 	return (
-		<div className="flex flex-col gap-8">
-			<div className="flex flex-row gap-4 mt-10 ml-10">
-				<Link href="/clients">Clients</Link>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					strokeWidth={1.5}
-					stroke="currentColor"
-					className="size-5"
-				>
-					<path
-						strokeLinecap="round"
-						strokeLinejoin="round"
-						d="m8.25 4.5 7.5 7.5-7.5 7.5"
-					/>
-				</svg>
-				<span>Create client</span>
-			</div>
-			<ul className="steps steps-vertical lg:steps-horizontal w-300 bg-red-500">
-				<li className={`step ${currentStep >= 1 ? "step-primary" : ""}`}>
-					General Settings
-				</li>
-				<li className={`step ${currentStep >= 2 ? "step-primary" : ""}`}>
-					Capability Config
-				</li>
-				<li className={`step ${currentStep >= 3 ? "step-primary" : ""}`}>
-					Login Settings
-				</li>
-			</ul>
-
-			<div className="ml-20">
-				{currentStep === 1 && <GeneralSettings />}
-				{currentStep === 2 && <CapabilityConfig />}
-				{currentStep === 3 && <LoginSettings />}
+		<div className="flex flex-col">
+			<div className="breadcrumbs p-10">
+				<ul>
+					<li>
+						<a href="/clients">Clients</a>
+					</li>
+					<li>Create Client</li>
+				</ul>
 			</div>
 
-			<div className="flex flex-row gap-14 ml-20">
-				<div className="flex flex-row gap-4">
-					<button
-						className="btn"
-						onClick={() => setCurrentStep(Math.max(currentStep - 1, 1))}
-					>
-						Previous
-					</button>
-					<button
-						className="btn"
-						onClick={() => setCurrentStep(Math.min(currentStep + 1, 3))}
-					>
-						Next
+			<div className="divider divider-vertical m-0 h-0" />
+
+			<div className="flex flex-col gap-10 p-10">
+				<div className="flex flex-row gap-5 items-start">
+					<ul className="steps steps-vertical text-sm gap-2">
+						<li className={`step ${currentStep >= 1 ? "step-primary" : ""}`}>
+							General Settings
+						</li>
+						<li className={`step ${currentStep >= 2 ? "step-primary" : ""}`}>
+							Capability Config
+						</li>
+						<li className={`step ${currentStep >= 3 ? "step-primary" : ""}`}>
+							Login Settings
+						</li>
+					</ul>
+
+					<div className="flex-1">
+						{currentStep === 1 && <GeneralSettings />}
+						{currentStep === 2 && <CapabilityConfig />}
+						{currentStep === 3 && <LoginSettings />}
+					</div>
+				</div>
+
+				<div className="flex flex-row gap-14">
+					<div className="flex flex-row gap-4">
+						<button
+							className="btn"
+							onClick={() => setCurrentStep(Math.max(currentStep - 1, 1))}
+						>
+							Previous
+						</button>
+						<button
+							className="btn"
+							onClick={() => setCurrentStep(Math.min(currentStep + 1, 3))}
+						>
+							Next
+						</button>
+					</div>
+					<button className="btn" onClick={() => redirect("/clients")}>
+						Cancel
 					</button>
 				</div>
-				<button className="btn" onClick={() => redirect("/clients")}>
-					Cancel
-				</button>
 			</div>
 		</div>
 	);
